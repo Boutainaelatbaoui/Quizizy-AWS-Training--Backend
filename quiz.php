a8a643e33ebe345decd5f35ce4f1455a20ffb946
Boutaina <boutaina.elatbaoui@gmail.com>
2023-01-03 11:49:36 +0100

    modified files

--- quiz.php
+++ quiz.php
@@ -6,13 +6,9 @@ class Quizizy extends Connection
     public function getData()
     {
         $sql = "SELECT q.*, 
-                MIN(CASE WHEN o.id = (q.id*4)-3 THEN o.id END) AS id_1,
                 MIN(CASE WHEN o.id = (q.id*4)-3 THEN o.answer END) AS answer_1, 
-                MIN(CASE WHEN o.id = (q.id*4)-2 THEN o.id END) AS id_2,
                 MAX(CASE WHEN o.id = (q.id*4)-2 THEN o.answer END) AS answer_2, 
-                MIN(CASE WHEN o.id = (q.id*4)-1 THEN o.id END) AS id_3,
                 MIN(CASE WHEN o.id = (q.id*4)-1 THEN o.answer END) AS answer_3,
-                MIN(CASE WHEN o.id = (q.id*4) THEN o.id END) AS id_4,
                 MAX(CASE WHEN o.id = (q.id*4) THEN o.answer END) AS answer_4
                 FROM questions q JOIN options o GROUP by q.id ORDER by q.id";
 
@@ -21,7 +17,7 @@ class Quizizy extends Connection
         $result = $stmt->fetchAll();
         // print_r(json_encode($result));
         $encoded_quiz = json_encode($result);
-        file_put_contents("data.json", $encoded_quiz);
+        file_put_contents("./assets/js/data.json", $encoded_quiz);
         print_r(file_put_contents("data.json", $encoded_quiz));
     }
 }
